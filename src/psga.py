1588ad4fb6b7e1e4c0045f4c89a349cc1d6d44d2
F M <psga@mikmak.fun>
2023-11-16 09:34:44 +0100

    Fix Optional for older python version

--- src/psga.py
+++ src/psga.py
@@ -3,7 +3,7 @@
 """Minimalistic Controller (as in the MVC paradigm) for PySimpleGUI."""
 
 import functools
-from typing import Callable, Dict, Protocol
+from typing import Callable, Dict, Optional, Protocol
 from typing_extensions import Self
 
 import PySimpleGUI as sg
@@ -18,7 +18,7 @@ class Action(Protocol):
         """"""
 
 
-def action(key: str | None = None):
+def action(key: Optional[str] = None):
     """Turns an event handler into an action using given key as event's name"""
 
     action._counter = getattr(action, "_counter", 0) + 1
@@ -81,7 +81,7 @@ class Dispatcher:
 class Controller:
     """Groups and registers actions to a dispatcher."""
 
-    def __init__(self, dispatcher: Dispatcher, window: sg.Window | None = None):
+    def __init__(self, dispatcher: Dispatcher, window: Optional[sg.Window] = None):
         self.window = window
         for method in [
             func
